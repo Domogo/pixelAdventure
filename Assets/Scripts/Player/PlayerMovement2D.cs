0830e7c0d53592e3d91b4fff3c3c3efc53f74f03
Domogo <domsatrapa@gmail.com>
2021-01-26 21:40:39 +0100

    pixelAdventure initial commit

---
+++ Assets/Scripts/Player/PlayerMovement2D.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour 
+{ 
+    public CharacterController2D controller;
+    public float runSpeed = 80f;
+
+    private readonly string HORIZONTAL_AXIS = "Horizontal";
+    private float horizontalMove = 0f;
+    private bool isPlayerJumping;
+
+    // Update is called once per frame
+    void Update() 
+    {
+        horizontalMove = Input.GetAxisRaw(HORIZONTAL_AXIS) * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            isPlayerJumping = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, isPlayerJumping);
+        isPlayerJumping = false;
+    }
+}
